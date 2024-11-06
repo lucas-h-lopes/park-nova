029e0225fe0a1d10cbd89db79a39fdf7de9f92fa
Lucas <lucas.hen.lps@gmail.com>
2024-11-06 19:35:15 -0300

    feat: controller class (POST, GETID, GETALL and DELETE methods)

---
+++ src/main/java/api_gestao_estacionamento/projeto/web/controller/UserController.java
@@ -0,0 +1,56 @@
+package api_gestao_estacionamento.projeto.web.controller;
+
+import api_gestao_estacionamento.projeto.model.User;
+import api_gestao_estacionamento.projeto.repository.projection.UserProjection;
+import api_gestao_estacionamento.projeto.service.UserService;
+import api_gestao_estacionamento.projeto.web.dto.PageableDto;
+import api_gestao_estacionamento.projeto.web.dto.UserCreateDto;
+import api_gestao_estacionamento.projeto.web.dto.UserResponseDto;
+import api_gestao_estacionamento.projeto.web.mapper.PageableMapper;
+import api_gestao_estacionamento.projeto.web.mapper.UserMapper;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/v1/users")
+public class UserController {
+
+    private final UserService userService;
+
+    @PostMapping
+    public ResponseEntity<UserResponseDto> insert(@RequestBody @Valid UserCreateDto dto) {
+        User user = UserMapper.toUser(dto);
+        userService.insert(user);
+        UserResponseDto result = UserMapper.toDto(user);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(user.getId()).toUri();
+        return ResponseEntity.created(uri).body(result);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<UserResponseDto> getById(@PathVariable Long id){
+        User user = userService.findUserById(id);
+        return ResponseEntity.ok(UserMapper.toDto(user));
+    }
+
+    @GetMapping
+    public ResponseEntity<PageableDto> getAll(Pageable pageable){
+        Page<UserProjection> projection = userService.findAllUsers(pageable);
+        return ResponseEntity.ok(PageableMapper.toDto(projection));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteById(@PathVariable Long id){
+        userService.deleteUserById(id);
+        return ResponseEntity.noContent().build();
+    }
+}

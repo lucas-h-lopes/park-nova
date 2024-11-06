f0e2daa182e53852d6e62d3bac07833eac357259
Lucas <lucas.hen.lps@gmail.com>
2024-11-06 19:29:20 -0300

    feat: add repository method to findAll with pagination

--- src/main/java/api_gestao_estacionamento/projeto/repository/UserRepository.java
+++ src/main/java/api_gestao_estacionamento/projeto/repository/UserRepository.java
@@ -1,7 +1,14 @@
 package api_gestao_estacionamento.projeto.repository;
 
 import api_gestao_estacionamento.projeto.model.User;
+import api_gestao_estacionamento.projeto.repository.projection.UserProjection;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface UserRepository extends JpaRepository<User, Long> {
+
+    @Query("select u from User u")
+    Page<UserProjection> findAllPageable(Pageable pageable);
 }

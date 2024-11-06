e4492dd6a0e16d5fab17a21a95100488129d8789
Lucas <lucas.hen.lps@gmail.com>
2024-11-06 16:11:18 -0300

    chore: initial setup and gitignore

---
+++ src/test/java/api_gestao_estacionamento/projeto/ProjetoApplicationTests.java
@@ -0,0 +1,13 @@
+package api_gestao_estacionamento.projeto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

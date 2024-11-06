448e419c76716b9ce1a63aa003ebc00853b32ae7
Lucas <lucas.hen.lps@gmail.com>
2024-11-06 19:34:17 -0300

    feat: add UserMapper

---
+++ src/main/java/api_gestao_estacionamento/projeto/web/mapper/UserMapper.java
@@ -0,0 +1,19 @@
+package api_gestao_estacionamento.projeto.web.mapper;
+
+import api_gestao_estacionamento.projeto.model.User;
+import api_gestao_estacionamento.projeto.web.dto.UserCreateDto;
+import api_gestao_estacionamento.projeto.web.dto.UserResponseDto;
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public class UserMapper {
+
+    public static User toUser(UserCreateDto dto) {
+        return new User(dto);
+    }
+
+    public static UserResponseDto toDto(User user) {
+        return new UserResponseDto(user);
+    }
+}

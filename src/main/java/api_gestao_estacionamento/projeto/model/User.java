6c72609e256cfe41324f29f0ca2ee432ea7267c3
Lucas <lucas.hen.lps@gmail.com>
2024-11-06 18:12:50 -0300

    feat: User entity

---
+++ src/main/java/api_gestao_estacionamento/projeto/model/User.java
@@ -0,0 +1,59 @@
+package api_gestao_estacionamento.projeto.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.io.Serializable;
+import java.time.LocalDateTime;
+import java.util.Objects;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "users")
+public class User implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(name = "username", nullable = false, unique = true, length = 150)
+    private String username;
+    @Column(name = "password", nullable = false, length = 200)
+    private String password;
+    @Enumerated(EnumType.STRING)
+    @Column(name = "role", nullable = false)
+    private Role role = Role.ROLE_CLIENT;
+
+    public enum Role {
+        ROLE_CLIENT, ROLE_ADMIN
+    }
+
+    @Column(name = "created_by")
+    private String createdBy;
+    @Column(name = "created_at")
+    private LocalDateTime createdAt;
+    @Column(name = "last_modified_by")
+    private String lastModifiedBy;
+    @Column(name = "last_modified_at")
+    private LocalDateTime lastModifiedAt;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return Objects.equals(id, user.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
+
+
+}

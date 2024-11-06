95800fce698793081829849a544d66e92065e354
Lucas <lucas.hen.lps@gmail.com>
2024-11-06 19:34:04 -0300

    feat: add PageableMapper

---
+++ src/main/java/api_gestao_estacionamento/projeto/web/mapper/PageableMapper.java
@@ -0,0 +1,14 @@
+package api_gestao_estacionamento.projeto.web.mapper;
+
+import api_gestao_estacionamento.projeto.web.dto.PageableDto;
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+import org.springframework.data.domain.Page;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public class PageableMapper {
+
+    public static PageableDto toDto(Page page){
+        return new PageableDto(page);
+    }
+}

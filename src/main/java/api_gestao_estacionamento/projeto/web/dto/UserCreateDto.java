1634585aaa02ac88e7064aec6accd480327db9fd
Lucas <lucas.hen.lps@gmail.com>
2024-11-06 19:33:13 -0300

    feat: add UserCreateDto

---
+++ src/main/java/api_gestao_estacionamento/projeto/web/dto/UserCreateDto.java
@@ -0,0 +1,22 @@
+package api_gestao_estacionamento.projeto.web.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserCreateDto {
+    @NotBlank(message = "Username não pode ser nulo, vazio ou conter somente espaços em branco")
+    @Pattern(regexp = "^[A-Za-z0-9]{4,}@[A-Za-z0-9]{2,}\\.[A-Za-z]{2,}$", message = "Username precisa estar no seguinte formato: 'lucas@gmail.com'")
+    private String username;
+    @NotBlank(message = "Password não pode ser nulo, vazio ou conter somente espaços em branco")
+    @Size(min = 6, max = 15, message = "Password deve possuir de 6 a 15 caracteres")
+    private String password;
+}

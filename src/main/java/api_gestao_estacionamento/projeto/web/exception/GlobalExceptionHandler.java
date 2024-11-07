bdb684f0ed358b7433058b7e4da24c0e0a2f7001
Lucas <lucas.hen.lps@gmail.com>
2024-11-07 18:01:19 -0300

    feat: add passwordInvalid exception handling

--- src/main/java/api_gestao_estacionamento/projeto/web/exception/GlobalExceptionHandler.java
+++ src/main/java/api_gestao_estacionamento/projeto/web/exception/GlobalExceptionHandler.java
@@ -1,11 +1,13 @@
 package api_gestao_estacionamento.projeto.web.exception;
 
 import api_gestao_estacionamento.projeto.service.exception.EntityNotFoundException;
+import api_gestao_estacionamento.projeto.service.exception.PasswordInvalidPassword;
 import api_gestao_estacionamento.projeto.service.exception.UsernameUniqueViolationException;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.AccessDeniedException;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -33,5 +35,17 @@ public class GlobalExceptionHandler {
     public ResponseEntity<CustomExceptionBody> validationErrors(HttpServletRequest request, BindingResult result){
         CustomExceptionBody error = new CustomExceptionBody(request, HttpStatus.UNPROCESSABLE_ENTITY, "Dado(s) inválido(s)", result);
         return ResponseEntity.status(422).body(error);
+    }
+
+    @ExceptionHandler(AccessDeniedException.class)
+    public ResponseEntity<CustomExceptionBody> accessDeniedException(HttpServletRequest request){
+        CustomExceptionBody error = new CustomExceptionBody(request, HttpStatus.FORBIDDEN, "O usuário autenticado não possui permissão para acessar este recurso");
+        return ResponseEntity.status(403).body(error);
+    }
+
+    @ExceptionHandler(PasswordInvalidPassword.class)
+    public ResponseEntity<CustomExceptionBody> passwordInvalidException(PasswordInvalidPassword e, HttpServletRequest request){
+        CustomExceptionBody error = new CustomExceptionBody(request, HttpStatus.BAD_REQUEST, e.getMessage());
+        return ResponseEntity.status(400).body(error);
     }
 }

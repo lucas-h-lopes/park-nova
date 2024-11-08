f7f0367b7370bb16f7de22a4db90435dca814063
Lucas <lucas.hen.lps@gmail.com>
2024-11-08 17:46:58 -0300

    docs: add @Schema description for dtos

--- src/main/java/api_gestao_estacionamento/projeto/web/dto/authenticate/AuthenticateDto.java
+++ src/main/java/api_gestao_estacionamento/projeto/web/dto/authenticate/AuthenticateDto.java
@@ -1,5 +1,6 @@
 package api_gestao_estacionamento.projeto.web.dto.authenticate;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
@@ -15,8 +16,10 @@ import lombok.Setter;
 public class AuthenticateDto {
     @NotBlank(message = "Username não pode ser nulo, vazio ou conter somente espaços em branco")
     @Pattern(regexp = "^[A-Za-z0-9]{4,}@[A-Za-z0-9]{2,}\\.[A-Za-z]{2,}$", message = "Username precisa estar no seguinte formato: 'lucas@gmail.com'")
+    @Schema(description = "O nome de usuário deve ser um e-mail válido.", example = "lucas@gmail.com")
     private String username;
     @NotBlank(message = "Password não pode ser nulo, vazio ou conter somente espaços em branco")
     @Size(min = 6, max = 15, message = "Password deve possuir de 6 a 15 caracteres")
+    @Schema(description = "A senha do usuário, com entre 6 a 15 caracteres. Deve conter uma combinação de letras e números.", example = "senha123")
     private String password;
 }
--- src/main/java/api_gestao_estacionamento/projeto/web/dto/user/UserNewPasswordDto.java
+++ src/main/java/api_gestao_estacionamento/projeto/web/dto/user/UserNewPasswordDto.java
@@ -1,5 +1,6 @@
 package api_gestao_estacionamento.projeto.web.dto.user;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
@@ -12,10 +13,13 @@ public class UserNewPasswordDto {
 
     @NotBlank(message = "A senha atual não pode ser nula, vazia ou conter somente caracteres em branco")
     @Size(min = 6, max = 15, message = "A senha atual deve possuir de 6 a 15 caracteres")
+    @Schema(description = "Senha atual do usuário", example = "senhaAtual123")
     private String currentPassword;
+    @Schema(description = "Nova senha do usuário", example = "novaSenha123")
     @NotBlank(message = "A nova senha não pode ser nula, vazia ou conter somente caracteres em branco")
     @Size(min = 6, max = 15, message = "A nova senha deve possuir de 6 a 15 caracteres")
     private String newPassword;
+    @Schema(description = "Confirmação da nova senha", example = "novaSenha123")
     @NotBlank(message = "A senha de confirmação não pode ser nula, vazia ou conter somente caracteres em branco")
     @Size(min = 6, max = 15, message = "A senha de confirmação deve possuir de 6 a 15 caracteres")
     private String confirmationPassword;
